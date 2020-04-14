077d82cb55a60507126941b171b009a354c75047
majung2 <dlalwjd1417@likelion.org>
2020-04-14 14:36:30 +0900

    new

--- newproject/manage.py
+++ newproject/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newproject.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
